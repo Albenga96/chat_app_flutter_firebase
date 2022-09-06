c791cd8560f41089a41cd35ee8ce80c012987ae9
Albenga96 <albertoferroni.af@gmail.com>
2022-09-06 11:53:49 +0200

    Added time

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({
@@ -21,21 +23,39 @@ class ChatMessages extends StatelessWidget {
       alignment: map['sendby'] != FirebaseAuth.instance.currentUser!.uid
           ? Alignment.centerRight
           : Alignment.centerLeft,
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.blue,
-        ),
-        child: Text(
-          map['message'],
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-            color: Colors.white,
+      child: Column(
+        crossAxisAlignment:
+            map['sendby'] != FirebaseAuth.instance.currentUser!.uid
+                ? CrossAxisAlignment.end
+                : CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.blue,
+            ),
+            child: Text(
+              map['message'],
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14),
+            child: Text(
+              DateFormat.Hm().format((map['time'] as Timestamp).toDate()),
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
